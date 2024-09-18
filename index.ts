40e89ced2718c0eaee527659d47bcca4bf789290
Luiz Felipe <luiztrindade01@outlook.com>
2024-09-18 14:39:04 -0300

    db conected

--- index.ts
+++ index.ts
@@ -5,8 +5,12 @@ import helmet from 'helmet'
 import { limiter } from './utils/limiter'
 import 'dotenv/config'
 import router from './routes'
+import mongoose from 'mongoose'
 
 const app = express()
+const MONGO_URI = process.env.MONGO_URI!
+
+mongoose.connect(MONGO_URI)
 
 app.use(cors(corsOptions))
 app.use(helmet())
