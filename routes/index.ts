8e3d2448118fd719f9ab1a2714ba7ea82e26ab88
Luiz Felipe <luiztrindade01@outlook.com>
2024-10-14 17:12:42 -0300

    stripe api

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import auth from './auth'
 import post from './post'
+import stripe from './stripe'
 
 const router = Router()
 
 router.use('/auth', auth)
 router.use('/post', post)
+router.use('/stripe', stripe)
 
 export default router
