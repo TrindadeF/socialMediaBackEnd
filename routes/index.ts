e3b0033375e7a3a030a3813d76544124880af04e
Luiz Felipe <luiztrindade01@outlook.com>
2024-10-16 18:01:40 -0300

    s3 bucket conection

--- routes/index.ts
+++ routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from 'express'
 import auth from './auth'
 import post from './post'
 import checkoutRoute from './checkoutRoute'
-import stripe from './stripe'
+import { uploadSingle } from '../middleware/upload'
 
 const router = Router()
 
 router.use('/auth', auth)
 router.use('/post', post)
-router.use('/stripe', stripe)
 router.use('/checkout', checkoutRoute)
+router.use('/upload', uploadSingle)
 
 export default router
