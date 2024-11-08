9d6ee4378af6366524cb1b90a859c846d94fb9e0
Luiz Felipe <luiztrindade01@outlook.com>
2024-11-08 09:37:21 -0300

    ecosystem for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [
+        {
+            name: 'socialMediaBackEnd',
+            script: 'index',
+            env: {
+                PORT: process.env.PORT,
+                URL: process.env.URL,
+                MONGO_URI: process.env.MONGO_URI,
+                SALT: process.env.SALT,
+                SECURITY_KEY: process.env.SECURITY_KEY,
+                STRIPE_SECRET: process.env.SECURITY_KEY,
+                AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+                AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+                AWS_REGION: process.env.AWS_REGION,
+                AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+                STRIPE_ID_PLAN: process.env.STRIPE_ID_PLAN,
+                STRIPE_ID_PLAN_2: process.env.STRIPE_ID_PLAN_2,
+                STRIPE_ID_PLAN_3: process.env.STRIPE_ID_PLAN_3,
+                MAIL_USERNAME: process.env.MAIL_USERNAME,
+                MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+            },
+        },
+    ],
+}
