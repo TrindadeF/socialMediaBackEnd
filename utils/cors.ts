263f617df41823cce6a83eff6418abf4f07f3f17
Luiz Felipe <luiztrindade01@outlook.com>
2024-11-11 12:21:20 -0300

    cors settings

--- utils/cors.ts
+++ utils/cors.ts
@@ -1,7 +1,12 @@
 import { CorsOptions } from 'cors'
 
 export const corsOptions: CorsOptions = {
-    origin: ['http://nakedlove.eu', 'http://www.nakedlove.eu'],
+    origin: [
+        'http://nakedlove.eu',
+        'http://www.nakedlove.eu',
+        'https://nakedlove.eu',
+        'https://www.nakedlove.eu',
+    ],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
