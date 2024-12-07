f75224f7d9aada075fa9b538f15f8ae4d0638ee4
Luiz Felipe <luiztrindade01@outlook.com>
2024-12-07 12:19:19 -0300

    deletePosts bug fixing

--- controllers/secondFeed/deletePosts.ts
+++ controllers/secondFeed/deletePosts.ts
@@ -3,14 +3,18 @@ import PostModel from '../../models/secondFeed'
 
 export const deletePost = async (req: Request, res: Response) => {
     try {
-        const postId = req.params.postId;
-        
+        const postId = req.params.postid
+        console.log('ID recebido para exclusão:', postId)
 
         const post = await PostModel.findByIdAndDelete(postId)
+        console.log('Resultado da exclusão:', post)
+
         if (!post) {
+            console.warn('Post não encontrado')
             return res.status(404).json({ error: 'Post não encontrado' })
         }
 
+        console.log('Post deletado com sucesso')
         return res.status(200).json({ message: 'Post deletado com sucesso' })
     } catch (error) {
         console.error('Erro ao deletar o post:', error)
