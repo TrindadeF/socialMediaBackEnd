caba11d6e0a16bba1cf4662a1724fc9d6630480a
Luiz Felipe <luiztrindade01@outlook.com>
2024-12-17 18:42:21 -0300

    reset Token settings

--- controllers/users/resetPassword.ts
+++ controllers/users/resetPassword.ts
@@ -30,7 +30,7 @@ export const forgotPassword = async (
         user.resetPasswordExpires = Date.now() + 3600000
         await user.save()
 
-        const resetLink = `http://localhost:4200/reset-password/${token}`
+        const resetLink = `https://nakedlove.eu/api/reset-password/${token}`
         const mailOptions = {
             from: 'nakedlove.service@gmail.com',
             to: user.email,
