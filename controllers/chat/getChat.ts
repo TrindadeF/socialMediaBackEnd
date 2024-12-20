06ee9a82f4e7f12dfd560e72c19162a3d698a2d2
Andre Ramalho <ramalhompi@gmail.com>
2024-12-20 10:26:33 -0300

    chat

--- controllers/chat/getChat.ts
+++ controllers/chat/getChat.ts
@@ -1,33 +1,34 @@
-import { Request, Response } from 'express'
-import { chatModel } from '../../models/chat'
-import { userModel } from '../../models/users'
+import { Request, Response } from 'express';
+import { chatModel } from '../../models/chat';
+import { userModel } from '../../models/users';
 
 export const getOrCreateChatByParticipants = async (
     req: Request,
     res: Response
 ) => {
-    const { userId1, userId2 } = req.body
+    const { userId1, userId2 } = req.body;
 
     try {
         let chat = await chatModel
             .findOne({
                 participants: { $all: [userId1, userId2] },
             })
-            .populate('participants', 'nickname')
+            .populate('participants', 'nickname isAnonymous'); // Inclui isAnonymous
 
         if (!chat) {
             chat = await chatModel.create({
                 participants: [userId1, userId2],
                 messages: [],
-            })
+            });
         }
 
+        // Busca os participantes com os campos necessários
         const participants = await userModel.find(
             {
                 _id: { $in: chat.participants },
             },
-            'nickname'
-        )
+            'nickname isAnonymous' // Inclui isAnonymous no retorno
+        );
 
         res.status(200).json({
             chatId: chat._id,
@@ -35,9 +36,10 @@ export const getOrCreateChatByParticipants = async (
             participants: participants.map((p) => ({
                 id: p._id,
                 nickname: p.nickName,
+                isAnonymous: p.isAnonymous, // Inclui no retorno do participante
             })),
-        })
+        });
     } catch (error) {
-        res.status(500).json({ message: 'Erro ao buscar ou criar chat', error })
+        res.status(500).json({ message: 'Erro ao buscar ou criar chat', error });
     }
-}
+};
