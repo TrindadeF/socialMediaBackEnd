dd47c8ff13e86ca4014b3276bb653bca84f0ef16
Luiz Felipe <luiztrindade01@outlook.com>
2024-12-30 11:52:13 -0300

    limiter ajusting

--- routes/stripeWebHook.ts
+++ routes/stripeWebHook.ts
@@ -4,7 +4,9 @@ import {
     handleCheckoutSessionCompleted,
     handleSubscriptionSessionCompleted,
     handleCancelPlan,
+    handleCancelSubscription,
 } from '../utils/stripe'
+import { userModel } from '../models/users'
 
 const router = express.Router()
 
@@ -45,5 +47,43 @@ router.post(
         res.json({ received: true })
     }
 )
+
+router.post('/cancel-subscription', async (req: Request, res: Response) => {
+    try {
+        const { subscriptionId } = req.body
+
+        if (!subscriptionId) {
+            return res
+                .status(400)
+                .json({ error: 'Subscription ID is required' })
+        }
+
+        const updatedSubscription =
+            await handleCancelSubscription(subscriptionId)
+
+        const stripeCustomerId = updatedSubscription.customer as string
+        const user = await userModel.findOne({ stripeCustomerId })
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' })
+        }
+
+        user.stripeSubscriptionStatus = null
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            subscription: updatedSubscription,
+        })
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Error canceling subscription:', error.message)
+            return res.status(500).json({ error: error.message })
+        }
+
+        console.error('Unexpected error:', error)
+        return res.status(500).json({ error: 'Unexpected error occurred' })
+    }
+})
 
 export default router
--- utils/limiter.ts
+++ utils/limiter.ts
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit'
 
 export const limiter = rateLimit({
-    limit: 100,
-    windowMs: 10 * 60 * 1000,
+    limit: 1000,
+    windowMs: 15 * 60 * 1000,
     message: 'Too many requests',
     standardHeaders: true,
     legacyHeaders: false,
