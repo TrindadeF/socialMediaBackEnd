86aac4958900e65412f1a4449491ddc4ad79f35e
Luiz Felipe <luiztrindade01@outlook.com>
2025-01-06 12:26:17 -0300

    bug fixing

--- controllers/users/getNotifications.ts
+++ controllers/users/getNotifications.ts
@@ -6,67 +6,66 @@ import secondFeed from '../../models/secondFeed'
 export const getNotifications = async (
     req: Request,
     res: Response
-): Promise<void> => {
+): Promise<Response> => {
     try {
-        const users = await userModel.find().lean()
+        const userId = req.params.userId
 
-        const usersWithDetails = await Promise.all(
-            users.map(async (user) => {
-                const primaryPosts = await primaryFeed
-                    .find({ owner: user._id })
+        const user = await userModel.findById(userId).lean()
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' })
+        }
+
+        const primaryPosts = await primaryFeed.find({ owner: userId }).lean()
+        const likesOnPrimaryPosts = await Promise.all(
+            primaryPosts.map(async (post) => {
+                const usersWhoLiked = await userModel
+                    .find({ _id: { $in: post.likes } })
+                    .select('_id name profilePic')
                     .lean()
-                const secondPosts = await secondFeed
-                    .find({ owner: user._id })
-                    .lean()
-
-                const likesOnPrimaryPosts = await Promise.all(
-                    primaryPosts.map(async (post) => {
-                        const usersWhoLiked = await userModel
-                            .find({ _id: { $in: post.likes } })
-                            .lean()
-                        return {
-                            postId: post._id,
-                            likedBy: usersWhoLiked,
-                        }
-                    })
-                )
-
-                const likesOnSecondPosts = await Promise.all(
-                    secondPosts.map(async (post) => {
-                        const usersWhoLiked = await userModel
-                            .find({ _id: { $in: post.likes } })
-                            .lean()
-                        return {
-                            postId: post._id,
-                            likedBy: usersWhoLiked,
-                        }
-                    })
-                )
-
-                const profileLikes = await userModel
-                    .find({ _id: { $in: user.likes } })
-                    .lean()
-
-                const followers = await userModel
-                    .find({ _id: { $in: user.followers } })
-                    .lean()
-
                 return {
-                    ...user,
-                    primaryPosts,
-                    secondPosts,
-                    likesOnPrimaryPosts,
-                    likesOnSecondPosts,
-                    profileLikes,
-                    followers,
+                    postId: post._id,
+                    content: post.content,
+                    likedBy: usersWhoLiked,
                 }
             })
         )
 
-        res.status(200).json(usersWithDetails)
+        const secondPosts = await secondFeed.find({ owner: userId }).lean()
+        const likesOnSecondPosts = await Promise.all(
+            secondPosts.map(async (post) => {
+                const usersWhoLiked = await userModel
+                    .find({ _id: { $in: post.likes } })
+                    .select('_id name profilePic')
+                    .lean()
+                return {
+                    postId: post._id,
+                    content: post.content,
+                    likedBy: usersWhoLiked,
+                }
+            })
+        )
+
+        const profileLikes = await userModel
+            .find({ _id: { $in: user.likes } })
+            .select('_id name profilePic')
+            .lean()
+
+        const followers = await userModel
+            .find({ _id: { $in: user.followers } })
+            .select('_id name profilePic')
+            .lean()
+
+        const notifications = {
+            profileLikes,
+            likesOnPrimaryPosts,
+            likesOnSecondPosts,
+            followers,
+        }
+
+        return res.status(200).json(notifications)
     } catch (error) {
-        res.status(500).json({
-            message: 'Erro ao buscar usuários, posts e interações',
+        return res.status(500).json({
+            message: 'Erro ao buscar notificações do usuário',
             error,
         })
     }
